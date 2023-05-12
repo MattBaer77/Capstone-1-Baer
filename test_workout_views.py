300aa8829b23dc8ed342420666cb33e059302b0e
Matthew Baer <matt.baer77@gmail.com>
2023-05-11 21:34:45 -0400

    completed test_workout_views.py

--- test_workout_views.py
+++ test_workout_views.py
@@ -200,8 +200,17 @@ class WorkoutViewsTestCase(TestCase):
             self.assertIn("Join Routine and Get In The Game!", html)
             self.assertIn("Access unauthorized.", html)
 
-    # def test_add_workout_post(self):
-    #     """Test /workout/add route POST""""
+    def test_add_workout_post(self):
+        """Test /workout/add route POST"""
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.user1.id
+            
+            resp = c.post("/workout/add", data={"description" : "Workout Number 1"}, follow_redirects=True)
+
+            self.assertEqual(resp.status_code, 200)
+            html = resp.get_data(as_text=True)
+            self.assertIn("Add an Exercise Goal To Your Workout!", html)
 
     def test_view_workout_get(self):
         """Test /workout/id route"""
@@ -228,8 +237,17 @@ class WorkoutViewsTestCase(TestCase):
             self.assertIn("Join Routine and Get In The Game!", html)
             self.assertIn("Access unauthorized.", html)
 
-    # def test_copy_workout_post(self):
-    #     """Test /workout/id/copy route"""
+    def test_copy_workout_post(self):
+        """Test /workout/id/copy route"""
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.user1.id
+            
+            resp = c.post("/workout/1/copy", follow_redirects=True)
+
+            self.assertEqual(resp.status_code, 200)
+            html = resp.get_data(as_text=True)
+            self.assertIn("Edit Your Workout", html)
 
     def test_copy_workout_post_no_user(self):
         """Test /workout/id/copy route if no user logged in"""
@@ -267,7 +285,6 @@ class WorkoutViewsTestCase(TestCase):
             self.assertIn("Join Routine and Get In The Game!", html)
             self.assertIn("Access unauthorized.", html)
 
-
     def test_add_workout_goal_get_wrong_user(self):
         """Test /workout/id/goal-add route - produces correct form if correct user not signed in"""
         with self.client as c:
@@ -281,9 +298,18 @@ class WorkoutViewsTestCase(TestCase):
             self.assertIn("Let's do this test2user!", html)
             self.assertIn("Access unauthorized.", html)
     
-    # def test_add_workout_goal_post(self):
-    #     """Test /workout/id/goal-add route"""
-    
+    def test_add_workout_goal_post(self):
+        """Test /workout/id/goal-add route"""
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.user1.id
+
+            resp = c.post("/workout/1/goal-add", data={"workout_id" : "1", "exercise_id" : "1", "goal_reps" : "1", "goal_sets" : "2", "goal_time_sec" : "3", "goal_weight_lbs" : "4", "goal_distance_miles" : "5.1"},follow_redirects=True)
+
+            self.assertEqual(resp.status_code, 200)
+            html = resp.get_data(as_text=True)
+            self.assertIn("Add an Exercise Goal To Your Workout!", html)
+
     def test_add_workout_goal_post_no_user(self):
         """Test /workout/id/goal-add route - if user not signed in"""
         with self.client as c:
@@ -346,9 +372,18 @@ class WorkoutViewsTestCase(TestCase):
             self.assertIn("Let's do this test2user!", html)
             self.assertIn("Access unauthorized.", html)   
  
-    # def test_edit_workout_post(self):
-    #     """Test /workout/id/edit route"""
+    def test_edit_workout_post(self):
+        """Test /workout/id/edit route"""
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.user1.id
 
+            resp = c.post("/workout/1/edit", data={"description" : "Workout Number 1"}, follow_redirects=True)
+
+            self.assertEqual(resp.status_code, 200)
+            html = resp.get_data(as_text=True)
+            self.assertIn("Let's do this test1user!", html)
+        
     def test_edit_workout_post_no_user(self):
         """Test /workout/id/edit route - if user not logged in"""
         with self.client as c:
@@ -446,267 +481,3 @@ class WorkoutViewsTestCase(TestCase):
             html = resp.get_data(as_text=True)
             self.assertIn("Let's do this test2user!", html)
             self.assertIn("Access unauthorized.", html)
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # def test_user_signup_get(self):
-    #     """Can a user view sign up page"""
-
-    #     with self.client as c:
-
-    #         resp = c.get("/signup")
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-    #         self.assertIn('<button class="btn btn-primary my-4">Signup</button>', html)
-
-    # def test_user_signup_post_success(self):
-    #     """Can a user sign up"""
-
-    #     with self.client as c:
-
-    #         resp = c.post("/signup", data={"username" : "test3user", "email" : "test3@test.com", "password" : "testuser3"})
-
-    #         self.assertEqual(resp.status_code, 302)
-    #         html = resp.get_data(as_text=True)
-    #         self.assertIn('<a href="/">/</a>', html)
-    
-
-    # def test_user_signup_post_success_redirect(self):
-    #     """Is a user appropriately redirected if successful signup"""
-
-    #     with self.client as c:
-
-    #         resp = c.post("/signup", data={"username" : "test3user", "email" : "test3@test.com", "password" : "testuser3"}, follow_redirects=True)
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-    #         self.assertIn("Let's do this test3user!", html)
-
-    # def test_user_signup_post_already_logged_in_redirect(self):
-    #     """User redirected away from login page if logged in"""
-
-    #     with self.client as c:
-    #         with c.session_transaction() as sess:
-    #             sess[CURR_USER_KEY] = self.user1.id
-            
-    #         resp = c.get("/login", follow_redirects=True)
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-    #         self.assertIn('You are already logged in!', html)
-
-    # def test_user_signup_post_duplicate_username(self):
-    #     """Is a user appropriately redirected if using duplicate username"""
-
-    #     with self.client as c:
-
-            # resp = c.post("/signup", data={"username" : "test2user", "email" : "test3@test.com", "password" : "testuser3"}, follow_redirects=True)
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-    #         self.assertIn('Username or email already taken', html)
-
-    # def test_user_signup_post_duplicate_email(self):
-    #     """Is a user appropriately redirected if using duplicate email"""
-
-    #     with self.client as c:
-
-    #         resp = c.post("/signup", data={"username" : "test3user", "email" : "test2@test.com", "password" : "testuser3"}, follow_redirects=True)
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-    #         self.assertIn('Username or email already taken', html)
-
-    # def test_user_login_get_success(self):
-    #     """Can a user view sign up page"""
-
-    #     with self.client as c:
-
-    #         resp = c.get("/login")
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-    #         self.assertIn('<button class="btn btn-primary my-4">Login</button>', html)
-    
-    # def test_user_already_logged_in_get(self):
-    #     """User redirected away from login page if logged in"""
-
-    #     with self.client as c:
-    #         with c.session_transaction() as sess:
-    #             sess[CURR_USER_KEY] = self.user1.id
-            
-    #         resp = c.get("/login", follow_redirects=True)
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-    #         self.assertIn('You are already logged in!', html)
-
-    # def test_user_login_post_success(self):
-    #     """Can a user log in"""
-
-    #     with self.client as c:
-
-    #         resp = c.post("/login", data={"username" : "test1user", "password" : "HASHED_PASSWORD1"})
-
-    #         self.assertEqual(resp.status_code, 302)
-    #         html = resp.get_data(as_text=True)
-    #         self.assertIn('<a href="/">/</a>', html)
-
-    # def test_user_login_post_success_redirect(self):
-    #     """Can a user log in"""
-
-    #     with self.client as c:
-
-    #         resp = c.post("/login", data={"username" : "test1user", "password" : "HASHED_PASSWORD1"}, follow_redirects=True)
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-    #         self.assertIn("Let's do this test1user!", html)
-
-    # def test_user_login_post_wrong_username(self):
-    #     """Is a user redirected and informed if username is incorrect"""
-
-    #     with self.client as c:
-
-    #         resp = c.post("/login", data={"username" : "testuser", "password" : "HASHED_PASSWORD1"}, follow_redirects=True)
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-    #         self.assertIn('Invalid credentials.', html)
-
-
-    # def test_user_login_post_wrong_password(self):
-    #     """Is a user redirected and informed if password is incorrect"""
-
-    #     with self.client as c:
-
-    #         resp = c.post("/login", data={"username" : "test1user", "password" : "HASHED_PASSWORD_WRONG"}, follow_redirects=True)
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-    #         self.assertIn('Invalid credentials.', html)
-
-    # def test_user_logout(self):
-    #     """Can a user log out?"""
-
-    #     with self.client as c:
-
-    #         resp = c.post("/logout", follow_redirects=True)
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-    #         self.assertIn('Goodbye!', html)
-
-    # def test_user_edit_get_success(self):
-    #     """Test GET for the user edit page"""
-
-    #     with self.client as c:
-    #         with c.session_transaction() as sess:
-    #             sess[CURR_USER_KEY] = self.user1.id
-
-    #         resp = c.get("/user/edit")
-
-    #         self.assertEqual(resp.status_code, 200)
-
-    #         html = resp.get_data(as_text=True)
-
-    #         self.assertIn('<h1 class="mb-4">Edit Your Details</h1>', html)
-
-    # def test_user_edit_get_not_user(self):
-    #     """Test GET for the user edit page if user is not logged in"""
-
-    #     with self.client as c:
-    #         resp = c.get("/user/edit")
-
-    #         self.assertEqual(resp.status_code, 302)
-
-    # def test_user_edit_get_not_user_redirect(self):
-    #     """Test GET for the user edit page if user is not logged in - FOLLOW REDIRECT"""
-
-    #     with self.client as c:
-    #         resp = c.get("/user/edit", follow_redirects=True)
-
-    #         self.assertEqual(resp.status_code, 200)
-
-    #         html = resp.get_data(as_text=True)
-
-    #         self.assertIn('Access unauthorized', html)
-
-    # def test_user_edit_post(self):
-    #     """Test POST for the user edit page"""
-
-    #     with self.client as c:
-    #         with c.session_transaction() as sess:
-    #             sess[CURR_USER_KEY] = self.user1.id
-
-    #         resp = c.post("/user/edit", data={"username" : "Edited_test1user", "password" : "HASHED_PASSWORD1"}, follow_redirects=True)
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-
-    #         self.assertIn("Edited_test1user", html)
-
-    # def test_user_delete_post(self):
-    #     """Test POST for the user delete route"""
-
-    #     with self.client as c:
-    #         with c.session_transaction() as sess:
-    #             sess[CURR_USER_KEY] = self.user1.id
-
-    #         resp = c.post("/user/delete", follow_redirects=True)
-
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-
-    #         self.assertIn("Join Routine and Get In The Game!", html)
-
-    # def test_user_logged_in_root(self):
-    #     """Test GET for the / route if logged in"""
-
-    #     with self.client as c:
-    #         with c.session_transaction() as sess:
-    #             sess[CURR_USER_KEY] = self.user1.id
-
-    #         resp = c.get("/")
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-
-    #         self.assertIn("Let's do this test1user!", html)
-
-    # def test_user_logged_out_root(self):
-    #     """Test GET for the / route if logged out"""
-
-    #     with self.client as c:
-
-    #         resp = c.get("/")
-    #         self.assertEqual(resp.status_code, 200)
-    #         html = resp.get_data(as_text=True)
-
-    #         self.assertIn("Join Routine and Get In The Game!", html)
